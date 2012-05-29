0f6742d73e661db7d10da2a0a8df7ad8d4495725
Johannes Thoma <johannes.thoma@rise-world.com>
2012-05-29 17:43:45 +0200

    Added test file
    Works under rails only - TODO: Port to something that works standalone.

---
+++ test/libxml-to-hash_test.rb
@@ -0,0 +1,25 @@
+# TODO: Port this test to something that can be tested standalone
+# (that is without rails).
+# require 'rails'
+
+class Tools::FromXmlTest < ActiveSupport::TestCase
+  def test_tiss_xml_node_class
+    assert_equal "karin", TissXmlNode.new("karin", "zak").content
+    assert_equal "zak", TissXmlNode.new("karin", "zak").attributes
+  end
+
+  def test_from_xml
+    assert_equal({:karin => {} }, Hash.from_xml("<karin/>"))
+    assert_equal({:karin => {} }, Hash.from_xml("<karin></karin>"))
+    assert_equal({:karin => "zak" }, Hash.from_xml("<karin>zak</karin>"))
+    assert_equal({:karin => {:zak => "lebt" }}, Hash.from_xml("<karin zak=\"lebt\"></karin>"))
+    assert_equal({:karin => {:zak => "lebt" }}, Hash.from_xml("<karin><zak>lebt</zak></karin>"))
+    assert_equal({:karin => TissXmlNode.new({:zak => "nicht"}, {:zak => "lebt"})}, Hash.from_xml("<karin zak=\"lebt\"><zak>nicht</zak></karin>"))
+    assert_equal({:karin => {:zak => ["nicht", "schon"] }}, Hash.from_xml("<karin><zak>nicht</zak><zak>schon</zak></karin>"))
+    assert_equal({:karin => {:zak => "nicht", :lebt => "schon"}}, Hash.from_xml("<karin><zak>nicht</zak><lebt>schon</lebt></karin>"))
+    assert_equal({:karin => {:zak => "nicht", :lebt => "schon", :oder => {:was => "nicht" }}}, Hash.from_xml("<karin><zak>nicht</zak><lebt>schon</lebt><oder><was>nicht</was></oder></karin>"))
+    assert_equal({:karin => TissXmlNode.new("schon", {:zak => "lebt"})}, Hash.from_xml("<karin zak=\"lebt\">schon</karin>"))
+    assert_nothing_raised { Hash.from_xml("<karin") }
+    assert_nil Hash.from_xml("<karin")
+  end
+end
